e69b984786fb04bb06c8fdd736b2ff426e513fa5
Mokshith <mokshithm15@gmail.com>
2024-09-22 21:02:11 -0400

    Made UI a lot better and finished user auth

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,10 @@
-# main/urls.py
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='main/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='main/logged_out.html'), name='logout'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,20 @@
 # main/views.py
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import UserRegisterForm
 
 def index(request):
     return render(request, 'main/index.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Automatically log in the user after registration
+            return redirect('index')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'main/register.html', {'form': form})
+
+
