dedc05fffc30fc34f8ef0b781599a3a86c052245
Mokshith <mokshithm15@gmail.com>
2024-09-30 16:36:27 -0400

    bunch of changes

--- main/models.py
+++ main/models.py
@@ -1,11 +1,13 @@
-# models.py
+# main/models.py
 from django.db import models
 
 class Restaurant(models.Model):
-    name = models.CharField(max_length=200)
-    latitude = models.FloatField()
+    name = models.CharField(max_length=255)
+    cuisine_type = models.CharField(max_length=100)
+    latitude = models.FloatField()  # For distance calculations
     longitude = models.FloatField()
-    details = models.TextField()
+    rating = models.DecimalField(max_digits=3, decimal_places=2)  # Rating out of 5
+    distance = models.FloatField()  # Distance from a reference point (optional for now)
 
     def __str__(self):
         return self.name
